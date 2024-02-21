83bd8556512597f3daa3e219022007c676bc855c
Aniket Raj <75392672+aniketraz@users.noreply.github.com>
2024-02-22 05:12:19 +0530

    Committing files

---
+++ Atcoder/C++ 20 (gcc 12.2)/abc329_a/47681088.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+
+int main() {
+    // Input string
+    std::string S;
+    std::cin >> S;
+
+    // Print each character with a space
+    for (int i = 0; i < S.length(); ++i) {
+        std::cout << S[i] << " ";
+    }
+
+    return 0;
+}
