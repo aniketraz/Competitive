0b6fce19129d25f311b98ea8dd2d8edcd3d76591
Aniket Raj <75392672+aniketraz@users.noreply.github.com>
2024-02-22 05:12:17 +0530

    Committing files

---
+++ Atcoder/C++ 20 (gcc 12.2)/abc341_a/50346314.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void print(int N) {
+    for (int i = 0; i < N; ++i) {
+        cout << "10";
+    }
+    cout << "1" <<endl;
+}
+
+int main() {
+    int N;
+    cin >> N;
+    print(N);
+
+    return 0;
+}
